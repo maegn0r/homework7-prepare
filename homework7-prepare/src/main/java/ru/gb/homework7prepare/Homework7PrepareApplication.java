20c142ee4dba9f77f88b38be12732f24e3cb6391
maegn0r <novitskynick@gmail.com>
2022-05-29 23:07:58 +0300

    init

---
+++ homework7-prepare/src/main/java/ru/gb/homework7prepare/Homework7PrepareApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.homework7prepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework7PrepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework7PrepareApplication.class, args);
+	}
+// comment1
+}
---
+++ homework7-prepare/src/test/java/ru/gb/homework7prepare/Homework7PrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.homework7prepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework7PrepareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
